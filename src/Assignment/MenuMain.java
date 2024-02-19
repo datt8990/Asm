3bc5145be3af3d11b0681ee58813869cad5127a4
datt8990 <157869797+datt8990@users.noreply.github.com>
2024-02-19 22:33:17 +0700

    MenuMain.java

--- src/Assignment/MenuMain.java
+++ src/Assignment/MenuMain.java
@@ -65,7 +65,6 @@ public class MenuMain {
 
                 case 5:
                     System.out.println("Bạn đã chọn chức năng 5.");
-                    CN5(arrNV);
                     break;
 
                 case 6:
@@ -195,80 +194,6 @@ public class MenuMain {
         }
     }
 
-    static void CN5(ArrayList<NhanVien> arrnv) {
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Nhập mã nhân viên cần cập nhật: ");
-        boolean timThay = false;
-        int index = 0;
-        String maNV = sc.nextLine();
-
-        for (NhanVien nv : arrnv) {
-
-            if (nv.getMaNV().equalsIgnoreCase(maNV)) {
-                timThay = true;
-                System.out.println("Cập nhật thông tin nhân viên\n(ấn enter nếu muốn giữ lại thông tin cũ): ");
-                System.out.println("Mã nhân viên: " + nv.getMaNV() + ": ");
-                String maNVnew = sc.nextLine();
-
-                if (!maNVnew.equalsIgnoreCase("")) {
-                    System.out.println("Cập nhật mã nhân viên mới: " + maNVnew);
-                    nv.setMaNV(maNVnew);
-                }
-
-                System.out.println("Tên nhân viên: " + nv.getHoTen() + ": ");
-                String hoTennew = sc.nextLine();
-
-                if (!hoTennew.equalsIgnoreCase("")) {
-                    System.out.println("Cập nhật tên nhân viên mới: " + hoTennew);
-                    nv.setHoTen(hoTennew);
-                }
-
-                System.out.println("Lương nhân viên: " + nv.getLuong() + ": ");
-                String luongNew = sc.nextLine();
-
-                if (luongNew.equalsIgnoreCase("")) {
-                    System.out.println("Cập nhật lương mới: " + luongNew);
-                    nv.setLuong(Double.parseDouble(luongNew));
-                }
-
-                if (nv instanceof TruongPhong) {
-                    System.out.println("Lương trách nhiệm: " + ((TruongPhong) nv).getTrachNhiem());
-                    String trachNhiemnew = sc.nextLine();
-
-                    if (!trachNhiemnew.equalsIgnoreCase("")) {
-                        System.out.println("Cập nhật lương trách nhiệm mới: " + trachNhiemnew);
-                        ((TruongPhong) nv).setTrachNhiem(Double.parseDouble(trachNhiemnew));
-
-                    } else if (nv instanceof TiepThi) {
-                        System.out.println("Doanh số: " + ((TiepThi) nv).getDoanhso() + ": ");
-                        String doanhSonew = sc.nextLine();
-                        if (!doanhSonew.equalsIgnoreCase("")) {
-                            System.out.println("Cập nhật doanh số mới: " + doanhSonew);
-                            ((TiepThi) nv).setDoanhso(Double.parseDouble(doanhSonew));
-
-                        }
-                        System.out.println("Hoa hồng: " + ((TiepThi) nv).getHueHong() + ": ");
-                        String hoaHongnew = sc.nextLine();
-                        if (!hoaHongnew.equalsIgnoreCase("")) {
-                            System.out.println("Cập nhật hoa hồng: " + hoaHongnew);
-                            ((TiepThi) nv).setHueHong(Double.parseDouble(hoaHongnew));
-
-                        }
-                    }
-                }
-                arrnv.set(index, nv);
-            } else {
-                index++;
-            }
-
-        } //end for
-        if (!timThay) {
-            System.out.println("Không tìm thấy mã nhân viên: " + maNV);
-        }
-
-    }
 
     static void CN6(ArrayList<NhanVien> arrNV) {
 
